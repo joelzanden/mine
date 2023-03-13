e31650a834609965c9abd4d4612b904fdd0ee185
Joel Zandén <info@joelzanden.se>
2023-03-13 21:47:07 +0100

    Add --version flag

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use mine::run;
-use std::process;
+use std::{env, process};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 && args[1] == "--version" {
+        println!("{}", env!("CARGO_PKG_VERSION"));
+        process::exit(0);
+    }
+
     if let Err(e) = run() {
         println!("Application error: {e}");
         process::exit(1);
