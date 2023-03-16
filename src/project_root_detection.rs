1f7648540bedaafaa5d8f72b5887fb1fe70be850
Joel Zandén <info@joelzanden.se>
2023-03-16 08:31:30 +0100

    Make DETECTABLE_PROJECT_FILES a pub static

--- src/project_root_detection.rs
+++ src/project_root_detection.rs
@@ -1,6 +1,103 @@
 use std::path::Path;
 use std::path::PathBuf;
 
+pub static DETECTABLE_PROJECT_FILES: [&str; 59] = [
+    // General
+    ".vscode",
+    ".git",
+    ".github",
+    ".gitlab-ci.yml",
+    ".editorconfig",
+    ".gitignore",
+    "README.md",
+    "LICENSE",
+    "Dockerfile",
+    ".dockerignore",
+    "Vagrantfile",
+    ".travis.yml",
+    "Jenkinsfile",
+    // Rust
+    "Cargo.toml",
+    // Python
+    "pyproject.toml",
+    "requirements.txt",
+    "setup.py",
+    "Pipfile",
+    // Node
+    "package.json",
+    "yarn.lock",
+    "lerna.json",
+    // Java
+    "pom.xml",
+    "build.gradle",
+    // C#
+    "project.json",
+    ".csproj",
+    ".fsproj",
+    ".sln",
+    // C++
+    "CMakeLists.txt",
+    "Makefile",
+    // Go
+    "go.mod",
+    // PHP
+    "composer.json",
+    // Ruby
+    "Gemfile",
+    ".gemspec",
+    "Rakefile",
+    // Swift
+    "Package.swift",
+    // Haskell
+    "stack.yaml",
+    // Lua
+    "lua",
+    // Perl
+    "cpanfile",
+    // Scala
+    "build.sbt",
+    // Elixir
+    "mix.exs",
+    // Elm
+    "elm.json",
+    // Erlang
+    "rebar.config",
+    // Dart
+    "pubspec.yaml",
+    // Julia
+    "Project.toml",
+    // Crystal
+    "shard.yml",
+    // Nim
+    "nim.cfg",
+    // OCaml
+    "dune",
+    // R
+    "DESCRIPTION",
+    // F#
+    "paket.dependencies",
+    // Clojure
+    "project.clj",
+    // Groovy
+    "build.gradle",
+    // PowerShell
+    "psakefile.ps1",
+    // TypeScript
+    "tsconfig.json",
+    // Vue
+    "vue.config.js",
+    // Angular
+    "angular.json",
+    // Svelte
+    "svelte.config.js",
+    // Ember
+    "ember-cli-build.js",
+    // Meteor
+    "meteor",
+    // ASP.NET
+    "project.json",
+];
+
 pub(crate) fn path_is_separator(path: &Path) -> bool {
     path.parent()
         .map_or(false, |parent| path.as_os_str() == parent.as_os_str())
@@ -21,102 +118,7 @@ pub fn get_project_dir_path() -> Result<PathBuf, &'static str> {
 }
 
 pub fn we_are_in_a_project_dir(path: &Path) -> bool {
-    let project_files = [
-        // General
-        ".vscode",
-        ".git",
-        ".github",
-        ".gitlab-ci.yml",
-        ".editorconfig",
-        ".gitignore",
-        "README.md",
-        "LICENSE",
-        "Dockerfile",
-        ".dockerignore",
-        "Vagrantfile",
-        ".travis.yml",
-        "Jenkinsfile",
-        // Rust
-        "Cargo.toml",
-        // Python
-        "pyproject.toml",
-        "requirements.txt",
-        "setup.py",
-        "Pipfile",
-        // Node
-        "package.json",
-        "yarn.lock",
-        "lerna.json",
-        // Java
-        "pom.xml",
-        "build.gradle",
-        // C#
-        "project.json",
-        ".csproj",
-        ".fsproj",
-        ".sln",
-        // C++
-        "CMakeLists.txt",
-        "Makefile",
-        // Go
-        "go.mod",
-        // PHP
-        "composer.json",
-        // Ruby
-        "Gemfile",
-        ".gemspec",
-        "Rakefile",
-        // Swift
-        "Package.swift",
-        // Haskell
-        "stack.yaml",
-        // Lua
-        "lua",
-        // Perl
-        "cpanfile",
-        // Scala
-        "build.sbt",
-        // Elixir
-        "mix.exs",
-        // Elm
-        "elm.json",
-        // Erlang
-        "rebar.config",
-        // Dart
-        "pubspec.yaml",
-        // Julia
-        "Project.toml",
-        // Crystal
-        "shard.yml",
-        // Nim
-        "nim.cfg",
-        // OCaml
-        "dune",
-        // R
-        "DESCRIPTION",
-        // F#
-        "paket.dependencies",
-        // Clojure
-        "project.clj",
-        // Groovy
-        "build.gradle",
-        // PowerShell
-        "psakefile.ps1",
-        // TypeScript
-        "tsconfig.json",
-        // Vue
-        "vue.config.js",
-        // Angular
-        "angular.json",
-        // Svelte
-        "svelte.config.js",
-        // Ember
-        "ember-cli-build.js",
-        // Meteor
-        "meteor",
-        // ASP.NET
-        "project.json",
-    ];
-
-    project_files.iter().any(|file| path.join(file).exists())
+    DETECTABLE_PROJECT_FILES
+        .iter()
+        .any(|file| path.join(file).exists())
 }
